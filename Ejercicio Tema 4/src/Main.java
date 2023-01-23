b8913b295d97ea4db07228c5cea49335e232ba74
JUAN JOSE NAVIA <naviajuanjo@gmail.com>
2023-01-23 18:14:09 -0500

    Ejercicio if resuelto

---
+++ Ejercicio Tema 4/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        var numeroIf = -3;
+
+        if (numeroIf < 0) {
+            System.out.println("El numeroIf es negativo");
+        } else if (numeroIf > 0) {
+            System.out.print("El numeroIf es positivo");
+        } else {
+            System.out.print("El numeroIf es cero (0)");
+        }
+    }
+}
